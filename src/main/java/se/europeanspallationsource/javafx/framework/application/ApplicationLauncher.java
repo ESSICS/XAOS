1f6b1c33c0e92fc3530c41fb4e62ec4d457b11c5
Claudio Rosati <Claudio.Rosati@esss.se>
2018-02-09 16:52:35 +0100

    Started implementing the essential classes.

---
+++ src/main/java/se/europeanspallationsource/javafx/framework/application/ApplicationLauncher.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2018 European Spallation Source ERIC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.europeanspallationsource.javafx.framework.application;
+
+
+import javafx.stage.Stage;
+import org.jacpfx.minimal.launcher.JacpFXApplicationLauncher;
+import org.jacpfx.rcp.workbench.FXWorkbench;
+import se.europeanspallationsource.javafx.framework.application.workbench.ApplicationWorkbench;
+
+
+/**
+ * The main entry point of the framework.
+ *
+ * @author claudio.rosati@esss.se
+ */
+public class ApplicationLauncher extends JacpFXApplicationLauncher {
+
+    @Override
+    protected final String[] getBasePackages() {
+        return new String[] { "se.europeanspallationsource.javafx.framework.application.workbench" };
+    }
+
+    @Override
+    protected final Class<? extends FXWorkbench> getWorkbenchClass() {
+        return ApplicationWorkbench.class;
+    }
+
+    /**
+     * Will be executed after JavaFX initialisation. Current implementation is
+     * empty, and can be safely overridden.
+     *
+     * @param stage The JavaFX {@link Stage}.
+     */
+    @Override
+    protected void postInit( Stage stage ) {
+    }
+
+}
---
+++ src/main/java/se/europeanspallationsource/javafx/framework/application/workbench/ApplicationWorkbench.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2018 European Spallation Source ERIC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.europeanspallationsource.javafx.framework.application.workbench;
+
+
+import javafx.event.Event;
+import javafx.scene.Node;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.jacpfx.api.annotations.workbench.Workbench;
+import org.jacpfx.api.componentLayout.WorkbenchLayout;
+import org.jacpfx.api.message.Message;
+import org.jacpfx.api.util.ToolbarPosition;
+import org.jacpfx.rcp.componentLayout.FXComponentLayout;
+import org.jacpfx.rcp.components.menuBar.JACPMenuBar;
+import org.jacpfx.rcp.workbench.FXWorkbench;
+
+import static se.europeanspallationsource.javafx.framework.application.workbench.Configuration.PERSPECTIVE_MAIN;
+
+
+/**
+ * The application workbench, where perspectives are registered and installed.
+ *
+ * @author claudio.rosati@esss.se
+ */
+@Workbench(
+     id = "application.workbench",
+     perspectives = { PERSPECTIVE_MAIN }
+)
+public class ApplicationWorkbench implements FXWorkbench {
+
+    @Override
+    public void handleInitialLayout( Message<Event, Object> action, WorkbenchLayout<Node> layout, Stage stage ) {
+
+//  TODO: CR: Initial size should be declared through the ApplicationLauncher
+//            constructor's parameters, or using an @ApplicationInfo annotation.
+//            This values should then be saved into application properties, and
+//            updated when the application closes.
+        layout.setWorkbenchXYSize(1024, 768);
+        layout.registerToolBar(ToolbarPosition.NORTH);
+        layout.setStyle(StageStyle.DECORATED);
+        layout.setMenuEnabled(true);
+        
+    }
+
+    @Override
+    public void postHandle( FXComponentLayout layout ) {
+
+        final JACPMenuBar menu = layout.getMenu();
+
+//  TODO:CR menus should be loaded dynamically, using a service.
+        Menu fileMenu = new Menu(Bundles.getLanguage().getString("menubar.file"));
+
+        menu.getMenus().addAll(fileMenu);
+
+    }
+
+}
