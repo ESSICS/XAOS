bdfc7fe529bfd2e6b480434ecbddee45018dc9f3
Claudio Rosati <Claudio.Rosati@esss.se>
2018-06-11 11:36:12 +0200

    Renamed a misspelled class name.

--- src/main/java/se/europeanspallationsource/xaos/components/TreeDirectoryAsynchronousIO.java
+++ src/main/java/se/europeanspallationsource/xaos/components/TreeDirectoryAsynchronousIO.java
@@ -33,12 +33,12 @@ import se.europeanspallationsource.xaos.tools.io.InitiatorAsynchronousIO;
  * @author claudio.rosati@esss.se
  * @see <a href="https://github.com/ESSICS/LiveDirsFX">LiveDirsFX:org.fxmisc.livedirs.LiveDirsIO</a>
  */
-public class TreeDirectoruAsynchronousIO<I> implements InitiatorAsynchronousIO<I> {
+public class TreeDirectoryAsynchronousIO<I> implements InitiatorAsynchronousIO<I> {
 
     private final Executor clientThreadExecutor;
 	private final DirectoryWatcher directoryWatcher;
 
-	public TreeDirectoruAsynchronousIO(
+	public TreeDirectoryAsynchronousIO(
 		DirectoryWatcher directoryWatcher,
 //		LiveDirsModel<I, ?> model,
 		Executor clientThreadExecutor
