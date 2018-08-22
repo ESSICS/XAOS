4b1f372e1303d06cfe8f75d9be271a1b512faec0
Claudio Rosati <Claudio.Rosati@esss.se>
2018-08-22 17:15:52 +0200

    Added the predicate interface.

--- xaos.ui.module/src/main/java/se/europeanspallationsource/xaos/ui/control/tree/TreeItemPredicate.java
+++ xaos.ui.module/src/main/java/se/europeanspallationsource/xaos/ui/control/tree/TreeItemPredicate.java
@@ -15,9 +15,46 @@
  */
 package se.europeanspallationsource.xaos.ui.control.tree;
 
+
+import java.util.function.Predicate;
+import javafx.scene.control.TreeItem;
+
+
 /**
+ * Represents a predicate (boolean-valued function) of two arguments: the first
+ * is the parent {@link TreeItem} of the element subject of the function call,
+ * and the second the value of element itself.
+ * <p>
+ * This is a {@link FunctionalInterface} whose functional method is
+ * {@link #test(TreeItem, Object)}.
+ *
+ * @param <T> The type of the {@link TreeItem#getValue() value} property within
+ *            {@link TreeItem}.
  * @author claudio.rosati@esss.se
  */
-public class TreeItemPredicate {
+@FunctionalInterface
+public interface TreeItemPredicate<T> {
+
+	/**
+	 * Evaluates this predicate on the given arguments.
+	 *
+	 * @param parent The parent {@link TreeItem} of the element or {@code null} if
+	 *               there is no parent.
+	 * @param value  The value to be tested.
+	 * @return {@code true} if the input argument matches the predicate.
+	 */
+	boolean test( TreeItem<T> parent, T value );
+
+	/**
+	 * Utility method to create a {@link TreeItemPredicate} from a given
+	 * {@link Predicate}.
+	 *
+	 * @param <T>       The type of the value.
+	 * @param predicate The {@link Predicate} to be wrapped.
+	 * @return A new {@link TreeItemPredicate} wrapped around the given {@code predicate}.
+	 */
+	static <T> TreeItemPredicate<T> create( Predicate<T> predicate ) {
+		return ( parent, value ) -> predicate.test(value);
+	}
 
 }
