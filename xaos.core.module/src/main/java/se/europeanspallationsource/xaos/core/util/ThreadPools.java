7168c389d9995aca81ce24fe7ea7181e6a826a15
Claudio Rosati <Claudio.Rosati@esss.se>
2019-06-20 10:30:02 +0200

    Improved ThreadPools.

--- xaos.core.module/src/main/java/se/europeanspallationsource/xaos/core/util/ThreadPools.java
+++ xaos.core.module/src/main/java/se/europeanspallationsource/xaos/core/util/ThreadPools.java
@@ -23,6 +23,7 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.logging.Logger;
 
 
 /**
@@ -33,11 +34,15 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 @SuppressWarnings( "ClassWithoutLogger" )
 public class ThreadPools {
 
+
 	private final static ReentrantReadWriteLock CACHED_THREAD_POOL_LOCK = new ReentrantReadWriteLock();
 	private final static ReentrantReadWriteLock FIXED_THREAD_POOL_LOCK = new ReentrantReadWriteLock();
+	private static final Logger LOGGER = Logger.getLogger(ThreadPools.class.getName());
 	private final static ReentrantReadWriteLock SINGLE_THREAD_EXECUTOR_LOCK = new ReentrantReadWriteLock();
-	private final static AtomicBoolean WAS_SHUTDOWN = new AtomicBoolean();
+	private final static AtomicBoolean WAS_REGISTERED = new AtomicBoolean(false);
+	private final static AtomicBoolean WAS_SHUTDOWN = new AtomicBoolean(false);
 	private final static ReentrantReadWriteLock WORK_STEALING_THREAD_POOL_LOCK = new ReentrantReadWriteLock();
+
 	private static ExecutorService cachedThreadPoolService = null;
 	private static ScheduledExecutorService fixedThreadPoolService = null;
 	private static ScheduledExecutorService singleThreadExecutorService = null;
@@ -66,17 +71,19 @@ public class ThreadPools {
 
 			if ( cachedThreadPoolService == null ) {
 
-				if ( WAS_SHUTDOWN.get() ) {
-					throw new IllegalStateException("Thread pools already shutdown.");
-				}
-
 				CACHED_THREAD_POOL_LOCK.readLock().unlock();
 				CACHED_THREAD_POOL_LOCK.writeLock().lock();
 
 				try {
-					cachedThreadPoolService = Executors.newCachedThreadPool(
-						r -> new Thread(r, "ThreadPools.cachedThreadPool.thread-" + Long.toHexString(System.nanoTime()))
-					);
+					if ( cachedThreadPoolService == null && !WAS_SHUTDOWN.get() ) {
+
+						cachedThreadPoolService = Executors.newCachedThreadPool(
+							r -> new Thread(r, "ThreadPools.cachedThreadPool.thread-" + Long.toHexString(System.nanoTime()))
+						);
+
+						registerShutdownHook();
+
+					}
 				} finally {
 					CACHED_THREAD_POOL_LOCK.readLock().lock();
 					CACHED_THREAD_POOL_LOCK.writeLock().unlock();
@@ -122,18 +129,20 @@ public class ThreadPools {
 
 			if ( fixedThreadPoolService == null ) {
 
-				if ( WAS_SHUTDOWN.get() ) {
-					throw new IllegalStateException("Thread pools already shutdown.");
-				}
-
 				FIXED_THREAD_POOL_LOCK.readLock().unlock();
 				FIXED_THREAD_POOL_LOCK.writeLock().lock();
 
 				try {
-					fixedThreadPoolService = Executors.newScheduledThreadPool(
-						Runtime.getRuntime().availableProcessors(),
-						r -> new Thread(r, "ThreadPools.fixedThreadPool.thread-" + Long.toHexString(System.nanoTime()))
-					);
+					if ( fixedThreadPoolService == null && !WAS_SHUTDOWN.get() ) {
+
+						fixedThreadPoolService = Executors.newScheduledThreadPool(
+							Runtime.getRuntime().availableProcessors(),
+							r -> new Thread(r, "ThreadPools.fixedThreadPool.thread-" + Long.toHexString(System.nanoTime()))
+						);
+
+						registerShutdownHook();
+
+					}
 				} finally {
 					FIXED_THREAD_POOL_LOCK.readLock().lock();
 					FIXED_THREAD_POOL_LOCK.writeLock().unlock();
@@ -154,65 +163,6 @@ public class ThreadPools {
 	 */
 	public static ForkJoinPool jdkCommonPool() {
 		return ForkJoinPool.commonPool();
-	}
-
-	/**
-	 * Initiates an orderly shutdown of all the thread pools, in which
-	 * previously submitted tasks are executed, but no new tasks will be
-	 * accepted.
-	 * <p>
-	 * This method does not wait for previously submitted tasks to complete
-	 * execution. Use {@code awaitTermination} on the thread pool you're
-	 * interested in to do that.</p>
-	 *
-	 * @see ExecutorService#shutdown()
-	 */
-	public static void shutdown() {
-
-		//	---- cached thread pool --------------------------------------------
-		CACHED_THREAD_POOL_LOCK.readLock().lock();
-
-		try {
-			if ( cachedThreadPoolService != null ) {
-				cachedThreadPoolService.shutdown();
-			}
-		} finally {
-			CACHED_THREAD_POOL_LOCK.readLock().unlock();
-		}
-
-		//	---- fixed thread pool ---------------------------------------------
-		FIXED_THREAD_POOL_LOCK.readLock().lock();
-
-		try {
-			if ( fixedThreadPoolService != null ) {
-				fixedThreadPoolService.shutdown();
-			}
-		} finally {
-			FIXED_THREAD_POOL_LOCK.readLock().unlock();
-		}
-
-		//	---- single thread executor ----------------------------------------
-		SINGLE_THREAD_EXECUTOR_LOCK.readLock().lock();
-
-		try {
-			if ( singleThreadExecutorService != null ) {
-				singleThreadExecutorService.shutdown();
-			}
-		} finally {
-			SINGLE_THREAD_EXECUTOR_LOCK.readLock().unlock();
-		}
-
-		//	---- work stealing thread pool -------------------------------------
-		WORK_STEALING_THREAD_POOL_LOCK.readLock().lock();
-
-		try {
-			if ( workStealingThreadPoolService == null ) {
-				workStealingThreadPoolService.shutdown();
-			}
-		} finally {
-			WORK_STEALING_THREAD_POOL_LOCK.readLock().unlock();
-		}
-
 	}
 
 	/**
@@ -238,17 +188,19 @@ public class ThreadPools {
 
 			if ( singleThreadExecutorService == null ) {
 
-				if ( WAS_SHUTDOWN.get() ) {
-					throw new IllegalStateException("Thread pools already shutdown.");
-				}
-
 				SINGLE_THREAD_EXECUTOR_LOCK.readLock().unlock();
 				SINGLE_THREAD_EXECUTOR_LOCK.writeLock().lock();
 
 				try {
-					singleThreadExecutorService = Executors.newSingleThreadScheduledExecutor(
-						r -> new Thread(r, "ThreadPools.singleThreadExecutor.thread-" + Long.toHexString(System.nanoTime()))
-					);
+					if ( singleThreadExecutorService == null && !WAS_SHUTDOWN.get() ) {
+
+						singleThreadExecutorService = Executors.newSingleThreadScheduledExecutor(
+							r -> new Thread(r, "ThreadPools.singleThreadExecutor.thread-" + Long.toHexString(System.nanoTime()))
+						);
+
+						registerShutdownHook();
+
+					}
 				} finally {
 					SINGLE_THREAD_EXECUTOR_LOCK.readLock().lock();
 					SINGLE_THREAD_EXECUTOR_LOCK.writeLock().unlock();
@@ -291,15 +243,19 @@ public class ThreadPools {
 
 			if ( workStealingThreadPoolService == null ) {
 
-				if ( WAS_SHUTDOWN.get() ) {
-					throw new IllegalStateException("Thread pools already shutdown.");
-				}
-
 				WORK_STEALING_THREAD_POOL_LOCK.readLock().unlock();
 				WORK_STEALING_THREAD_POOL_LOCK.writeLock().lock();
 
 				try {
-					workStealingThreadPoolService = Executors.newWorkStealingPool();
+					if ( workStealingThreadPoolService == null && !WAS_SHUTDOWN.get() ) {
+
+						workStealingThreadPoolService = Executors.newWorkStealingPool(
+							Runtime.getRuntime().availableProcessors()
+						);
+
+						registerShutdownHook();
+
+					}
 				} finally {
 					WORK_STEALING_THREAD_POOL_LOCK.readLock().lock();
 					WORK_STEALING_THREAD_POOL_LOCK.writeLock().unlock();
@@ -315,7 +271,80 @@ public class ThreadPools {
 
 	}
 
+	private static void registerShutdownHook() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+
+	/**
+	 * Initiates an orderly shutdown of all the thread pools, in which
+	 * previously submitted tasks are executed, but no new tasks will be
+	 * accepted.
+	 * <p>
+	 * This method does not wait for previously submitted tasks to complete
+	 * execution. Use {@code awaitTermination} on the thread pool you're
+	 * interested in to do that.</p>
+	 * <p>
+	 * This method doesn't need to be explicitly called, because automatically
+	 * done by a shutdown hook.</p>
+	 *
+	 * @see ExecutorService#shutdown()
+	 */
+	private static void shutdown() {
+
+		if ( WAS_SHUTDOWN.compareAndSet(false, true) ) {
+
+			//	---- cached thread pool ----------------------------------------
+			CACHED_THREAD_POOL_LOCK.writeLock().lock();
+
+			try {
+				if ( cachedThreadPoolService != null ) {
+					cachedThreadPoolService.shutdown();
+				}
+			} finally {
+				CACHED_THREAD_POOL_LOCK.writeLock().unlock();
+			}
+
+			//	---- fixed thread pool -----------------------------------------
+			FIXED_THREAD_POOL_LOCK.writeLock().lock();
+
+			try {
+				if ( fixedThreadPoolService != null ) {
+					fixedThreadPoolService.shutdown();
+				}
+			} finally {
+				FIXED_THREAD_POOL_LOCK.writeLock().unlock();
+			}
+
+			//	---- single thread executor ------------------------------------
+			SINGLE_THREAD_EXECUTOR_LOCK.writeLock().lock();
+
+			try {
+				if ( singleThreadExecutorService != null ) {
+					singleThreadExecutorService.shutdown();
+				}
+			} finally {
+				SINGLE_THREAD_EXECUTOR_LOCK.writeLock().unlock();
+			}
+
+			//	---- work stealing thread pool ---------------------------------
+			WORK_STEALING_THREAD_POOL_LOCK.writeLock().lock();
+
+			try {
+				if ( workStealingThreadPoolService != null ) {
+					workStealingThreadPoolService.shutdown();
+				}
+			} finally {
+				WORK_STEALING_THREAD_POOL_LOCK.writeLock().unlock();
+			}
+
+		}
+
+	}
+
 	private ThreadPools() {
+		if ( WAS_REGISTERED.compareAndSet(false, true) ) {
+			Runtime.getRuntime().addShutdownHook(new Thread(ThreadPools::shutdown, "ThreadPools Shutdown Hook"));
+		}
 	}
 
 }
