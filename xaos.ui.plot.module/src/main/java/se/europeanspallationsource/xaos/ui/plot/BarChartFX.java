2830be2bdd1d459e9a69a0b8e1284b0a5184d87e
Claudio Rosati <Claudio.Rosati@esss.se>
2019-07-01 16:11:19 +0200

    BarChart completed.

--- xaos.ui.plot.module/src/main/java/se/europeanspallationsource/xaos/ui/plot/BarChartFX.java
+++ xaos.ui.plot.module/src/main/java/se/europeanspallationsource/xaos/ui/plot/BarChartFX.java
@@ -1,5 +1,3 @@
-package se.europeanspallationsource.xaos.ui.plot;
-
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  * Copyright (C) 2018-2019 by European Spallation Source ERIC.
@@ -8,7 +6,7 @@ package se.europeanspallationsource.xaos.ui.plot;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,73 +14,106 @@ package se.europeanspallationsource.xaos.ui.plot;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package se.europeanspallationsource.xaos.ui.plot;
 
 
-
-import se.europeanspallationsource.xaos.ui.plot.Plugin;
-import se.europeanspallationsource.xaos.ui.plot.PluginManager;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.Chart;
-import se.europeanspallationsource.xaos.ui.plot.Legend;
+import javafx.scene.chart.ValueAxis;
+import org.apache.commons.lang3.Validate;
+import se.europeanspallationsource.xaos.core.util.LogUtils;
 import se.europeanspallationsource.xaos.ui.plot.Legend.LegendItem;
 import se.europeanspallationsource.xaos.ui.plot.plugins.Pluggable;
+import se.europeanspallationsource.xaos.ui.plot.util.SeriesColorUtils;
+
+import static java.util.logging.Level.WARNING;
+
 
 /**
- * A thin extension of the FX {@link AreaChart} supporting custom {@link Plugin} plugin implementations.
- * 
+ * A thin extension of the FX {@link BarChart} supporting custom
+ * {@link Plugin plugin} implementations.
+ *
  * @author Grzegorz Kruk
  * @param <X> type of X values
  * @param <Y> type of Y values
  */
 public class BarChartFX<X, Y> extends BarChart<X, Y> implements Pluggable {
 
-    //Variables used to include plugIns in teh Chart
-    private final Group pluginsNodesGroup = new Group();
-    private final PluginManager pluginManager = new PluginManager(this, pluginsNodesGroup);
-    
-    //Variables that controls the line that doens't show in the Legend or series that are not displayed at the chart at a given time
-    private final List<String> noShowInLegend = new ArrayList<>();
-    private final List<String> seriesDrawnInPlot = new ArrayList<>();
-   
-    // Variable that stored the color line setup
-    private String colorStyle = new String();
-    
-    /**
-     * Construct a new BarChart with the given axis and data. The two axis should be a ValueAxis/NumberAxis and a
-     * CategoryAxis, they can be in either order depending on if you want a horizontal or vertical bar chart.
-     *
-     * @param xAxis The x axis to use
-     * @param yAxis The y axis to use
-     * @see javafx.scene.chart.BarChart#BarChart(Axis, Axis)
-     */
-    public BarChartFX(Axis<X> xAxis, Axis<Y> yAxis) {
-        this(xAxis, yAxis, FXCollections.<Series<X, Y>> observableArrayList());
-    }
+	private static final Logger LOGGER = Logger.getLogger(BarChartFX.class.getName());
 
-    /**
-     * Construct a new BarChart with the given axis and data. The two axis should be a ValueAxis/NumberAxis and a
-     * CategoryAxis, they can be in either order depending on if you want a horizontal or vertical bar chart.
-     *
-     * @param xAxis The x axis to use
-     * @param yAxis The y axis to use
-     * @param data The data to use, this is the actual list used so any changes to it will be reflected in the chart
-     * @see javafx.scene.chart.BarChart#BarChart(Axis, Axis, ObservableList)
-     */
-    public BarChartFX(Axis<X> xAxis, Axis<Y> yAxis, ObservableList<Series<X, Y>> data) {
-        super(xAxis, yAxis, data);
-        getStylesheets().add("/styles/chart.css");
-        getPlotChildren().add(pluginsNodesGroup);
-    }
-    
-    
+	private final List<String> notShownInLegend = new ArrayList<>(4);
+	private final Group pluginsNodesGroup = new Group();
+	private final PluginManager pluginManager = new PluginManager(this, pluginsNodesGroup);
+	private final List<String> seriesDrawnInPlot = new ArrayList<>(4);
+
+	/**
+	 * Construct a new bar chart with the given axis. The two axis should be a
+	 * {@link ValueAxis}/{@link NumberAxis} and a {@link CategoryAxis}, they can
+	 * be in either order depending on if you want a horizontal or vertical bar
+	 * chart.
+	 *
+	 * @param xAxis The x axis to use.
+	 * @param yAxis The y axis to use.
+	 * @see javafx.scene.chart.BarChart#BarChart(Axis, Axis)
+	 */
+	public BarChartFX( Axis<X> xAxis, Axis<Y> yAxis ) {
+		this(xAxis, yAxis, FXCollections.<Series<X, Y>>observableArrayList());
+	}
+
+	/**
+	 * Construct a new bar chart with the given axis and data. The two axis
+	 * should be a {@link ValueAxis}/{@link NumberAxis} and a
+	 * {@link CategoryAxis}, they can be in either order depending on if you
+	 * want a horizontal or vertical bar chart.
+	 *
+	 * @param xAxis The x axis to use.
+	 * @param yAxis The y axis to use.
+	 * @param data  The data to use, this is the actual list used so any changes
+	 *              to it will be reflected in the chart.
+	 * @see javafx.scene.chart.BarChart#BarChart(Axis, Axis, ObservableList)
+	 */
+	public BarChartFX( Axis<X> xAxis, Axis<Y> yAxis, ObservableList<Series<X, Y>> data ) {
+
+		super(xAxis, yAxis, data);
+
+		getStylesheets().add(BarChartFX.class.getResource("/styles/chart.css").toExternalForm());
+		getPlotChildren().add(pluginsNodesGroup);
+
+	}
+
+	/**
+	 * More robust method for adding plugins to chart.
+	 * <p>
+	 * <b>Note:</b> Only necessary if more than one plugin is being added at
+	 * once.</p>
+	 *
+	 * @param plugins List of {@link Plugin}s to be added.
+	 */
+	public void addChartPlugins( ObservableList<Plugin> plugins ) {
+		plugins.forEach(plugin -> {
+			try {
+				pluginManager.getPlugins().add(plugin);
+			} catch ( Exception ex ) {
+				LogUtils.log(
+					LOGGER,
+					WARNING,
+					"Error occured whilst adding {0} [{1}].",
+					plugin.getClass().getName(),
+					ex.getMessage()
+				);
+			}
+		});
+
+	}
 
 	@Override
 	public Chart getChart() {
@@ -90,127 +121,12 @@ public class BarChartFX<X, Y> extends BarChart<X, Y> implements Pluggable {
 	}
 
 	@Override
-    public final ObservableList<Plugin> getPlugins() {
-        return pluginManager.getPlugins();
-    }
-    
-    /**
-     * More robust method for adding plugins to chart.
-     * Note: Only necessary if more than one plugin is being added at once. 
-     * 
-     * @param plugins list of XYChartPlugins to be added. 
-     * 
-     * 
-     */
-    public void addChartPlugins(ObservableList<Plugin> plugins){
-         plugins.forEach(item->{
-         try{
-            pluginManager.getPlugins().add(item);
-         }
-         catch (Exception e) {
-             System.out.println("Error occured whilst adding" + item.getClass().toString());
-         }
-         });
-        
-    }
-
-
-    @Override
-    protected void layoutPlotChildren() {
-        movePluginsNodesToFront();
-        super.layoutPlotChildren();
-    }
-
-    private void movePluginsNodesToFront() {
-        getPlotChildren().remove(pluginsNodesGroup);
-        getPlotChildren().add(pluginsNodesGroup);
-    }
-    
-     public final void setSeriesAsHorizontal(Integer index){        
-                        
-        colorStyle = colorStyle+"\n"+"-color"+index+": -xaos-chart-horizontal;";
-        this.lookup(".chart").setStyle(colorStyle);
-               
-    }
-    
-    public final void setSeriesAsVertical(Integer index){        
-                
-        colorStyle = colorStyle+"\n"+"-color"+index+": -xaos-chart-vertical;";
-        this.lookup(".chart").setStyle(colorStyle);  
-               
-    }
-    
-    public final void setSeriesAsLongitudinal(Integer index){        
-        
-        colorStyle = colorStyle+"\n"+"-color"+index+": -xaos-chart-longitudinal;";
-        this.lookup(".chart").setStyle(colorStyle);  
-               
-    }
-    
-	@Override
-    public final void setNotShownInLegend(String name){
-        noShowInLegend.add(name);
-        updateLegend();
-    }    
-    
-	@Override
-    public boolean isNotShownInLegend(String name){
-        return noShowInLegend.contains(name);
-    }
-    
-    public boolean isSeriesDrawn(String name){
-        return seriesDrawnInPlot.contains(name);
-    }
-
-     @Override
-    protected void updateLegend()
-    {
-//        final Legend legend = new Legend();
-//        seriesDrawnInPlot.clear();
-//        legend.getItems().clear();
-//        for (final Series<X, Y> series : getData())
-//        {
-//            if(!noShowInLegend.contains(series.getName())){
-//                Legend.LegendItem legenditem = new Legend.LegendItem(series.getName());
-//                final CheckBox cb = new CheckBox(series.getName());
-//                seriesDrawnInPlot.add(series.getName());
-//                cb.setUserData(series);
-//                cb.setSelected(true);
-//                //cb.setPadding(new Insets(0,10,0,0));
-//                cb.setStyle("-fx-text-fill: -color"+this.getData().indexOf(series)+" ;");
-//                cb.addEventHandler(ActionEvent.ACTION, e ->{
-//                    final CheckBox box = (CheckBox) e.getSource();
-//                    @SuppressWarnings("unchecked")
-//                    final Series<Number, Number> s = (Series<Number, Number>) box.getUserData();
-//                    s.getNode().setVisible(box.isSelected());
-//                    s.getData().forEach(data ->{
-//                        StackPane stackPane = (StackPane) data.getNode();
-//                        stackPane.setVisible(box.isSelected());
-//                    });
-//                    if(box.isSelected()){
-//                        if (!seriesDrawnInPlot.contains(s.getName())){
-//                            seriesDrawnInPlot.add(s.getName());
-//                        }
-//                    } else {
-//                        seriesDrawnInPlot.remove(s.getName());
-//                    }
-//                });
-//                legenditem.setText("");
-//                legenditem.setSymbol(cb);
-//                legend.getItems().add(legenditem);
-//            }
-//        }
-//        setLegend(legend);
-    }
-    
-    
-	@Override
 	public ObservableList<LegendItem> getLegendItems() {
 
 		Node legend = getLegend();
 
 		if ( legend instanceof Legend ) {
-			return ((Legend) legend).getItems();
+			return ( (Legend) legend ).getItems();
 		} else {
 			return FXCollections.emptyObservableList();
 		}
@@ -218,8 +134,122 @@ public class BarChartFX<X, Y> extends BarChart<X, Y> implements Pluggable {
 	}
 
 	@Override
-	public ObservableList<Node> getPlotChildren() {
+	public final ObservableList<Node> getPlotChildren() {
 		return super.getPlotChildren();
+	}
+
+	@Override
+	public final ObservableList<Plugin> getPlugins() {
+		return pluginManager.getPlugins();
+	}
+
+	@Override
+	public boolean isNotShownInLegend( String name ) {
+		return notShownInLegend.contains(name);
+	}
+
+	public boolean isSeriesDrawn( String name ) {
+		return seriesDrawnInPlot.contains(name);
+	}
+
+	/**
+	 * Sets which series has to be considered "horizontal", "vertical" and
+	 * "longitudinal". Special colors will be used to represent horizontal
+	 * (red), vertical (blue) and longitudinal (green) series.
+	 *
+	 * @param horizontal   Index of the horizontal series. Use -1 if no
+	 *                     horizontal series exists.
+	 * @param vertical     Index of the vertical series. Use -1 if no vertical
+	 *                     series exists.
+	 * @param longitudinal Index of the longitudinal series. Use -1 if no
+	 *                     longitudinal series exists.
+	 */
+	public final void setHVLSeries( int horizontal, int vertical, int longitudinal ) {
+
+		int size = getData().size();
+
+		Validate.isTrue(horizontal < size, "Out of range 'horizontal' parameter.");
+		Validate.isTrue(vertical < size, "Out of range 'vertical' parameter.");
+		Validate.isTrue(longitudinal < size, "Out of range 'longitudinal' parameter.");
+
+		lookup(".chart").setStyle(SeriesColorUtils.styles(horizontal, vertical, longitudinal));
+
+	}
+
+	@Override
+	public final void setNotShownInLegend( String name ) {
+		notShownInLegend.add(name);
+		updateLegend();
+	}
+
+	@Override
+	protected void layoutPlotChildren() {
+
+		//	Move plugins nodes to front.
+		ObservableList<Node> plotChildren = getPlotChildren();
+
+		plotChildren.remove(pluginsNodesGroup);
+		plotChildren.add(pluginsNodesGroup);
+
+		//	Layout plot children. This call will create fresh new symbols
+		//	that are by default visible.
+		super.layoutPlotChildren();
+
+		//	If the track is hidden, then hide the symbols.
+		getData().stream()
+			.filter(series -> !seriesDrawnInPlot.contains(series.getName()))
+			.flatMap(series -> series.getData().stream())
+			.forEach(d -> d.getNode().setVisible(false));
+
+	}
+
+	@Override
+	protected void updateLegend() {
+
+		final Legend legend = new Legend();
+
+		seriesDrawnInPlot.clear();
+
+		for ( int i = 0; i < getData().size(); i++ ) {
+
+			final int seriesIndex = i;
+			Series<X, Y> series = getData().get(seriesIndex);
+			String seriesName = series.getName();
+
+			if ( !notShownInLegend.contains(seriesName) ) {
+
+				Legend.LegendItem legenditem = new Legend.LegendItem(seriesName, selected -> {
+
+					series.getData().forEach(d -> d.getNode().setVisible(selected));
+
+					if ( selected ) {
+						if ( !seriesDrawnInPlot.contains(seriesName) ) {
+							seriesDrawnInPlot.add(seriesName);
+						}
+					} else {
+						seriesDrawnInPlot.remove(seriesName);
+					}
+
+					getPlugins().forEach(p -> p.seriesVisibilityUpdated(this, series, seriesIndex, selected));
+
+				});
+
+				legenditem.getSymbol().getStyleClass().addAll(
+					"chart-area-symbol",
+					"area-legend-symbol",
+					"default-color" + ( seriesIndex % 8 ),
+					"series" + seriesIndex
+				);
+
+				seriesDrawnInPlot.add(seriesName);
+				legend.getItems().add(legenditem);
+
+			}
+
+		}
+
+		setLegend(legend);
+
 	}
 
 }
